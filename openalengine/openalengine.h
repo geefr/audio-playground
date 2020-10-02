59f6d69c5afe38d0f9f4ba565460545578567aee
Gareth Francis <gfrancis.dev@gmail.com>
2020-10-02 17:25:58 +0100

    Switch to openal-soft on Windows

--- openalengine/openalengine.h
+++ openalengine/openalengine.h
@@ -1,13 +1,8 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#ifdef WIN32
-# include <al.h>
-# include <alc.h>
-#else
-# include <AL/al.h>
-# include <AL/alc.h>
-#endif
+#include <AL/al.h>
+#include <AL/alc.h>
 
 #include <string>
 #include <vector>
