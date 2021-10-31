b56aaf4cb5f8247559a835b8a19b923f2657b132
Gareth Francis <gfrancis.dev@gmail.com>
2021-10-31 11:01:40 +0000

    Add vcpkg for windows dependencies

---
+++ bootstrap-vcpkg.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+./vcpkg/bootstrap-vcpkg.sh -disableMetrics
+./vcpkg/vcpkg install
+
+rm -rf build
+mkdir build
+cd build
+
+cmake -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_INSTALL_PREFIX=../install ../
